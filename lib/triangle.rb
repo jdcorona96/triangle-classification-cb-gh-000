3e4c7e72a81e683dc0c690efc9f665709cdf10a6
Joseph Corona <josed.hc96@gmail.com>
2018-11-23 00:42:09 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,16 @@
 class Triangle
   # write code here
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+    
+  end
+
+  attr_accessor :a, :b, :c, :type
+
+  def kind
+    
+  end
 end
