6b4b1e64c5be0eafba0ce42ebb773a45e5737aeb
Joseph Corona <josed.hc96@gmail.com>
2018-11-23 00:50:47 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -5,13 +5,24 @@ class Triangle
     @a = a
     @b = b
     @c = c
-    
+
   end
 
   attr_accessor :a, :b, :c
   attr_reader :type
 
   def kind
-    
-  end
-end
+    if @a+@b > @c ||
+       @b+@c > @a ||
+       @a+@c > @b then
+
+       begin
+         raise TriangleError
+       rescue TriangleError => error
+         puts error.
+       end ## if_error
+
+     else
+     end ## if_else end
+  end ## def_kind
+end # class Triangle
